d3e74944f6797522d96d05bf6127b8e6adb57010
MoonKnight <39974442+moonknight998@users.noreply.github.com>
2025-11-07 18:51:37 +0700

    06:Background Jobs

--- src/app/logout.tsx
+++ src/app/logout.tsx
@@ -2,11 +2,23 @@
 
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 export const LogoutButton = () => {
-    return (
-        <Button onClick={() => authClient.signOut()}>
-            Logout
-        </Button>
-    );
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await authClient.signOut(); // Đăng xuất
+      router.push("/login"); // Chuyển hướng về trang login
+    } catch (error) {
+      console.error("Đăng xuất thất bại:", error);
+    }
+  };
+
+  return (
+    <Button onClick={handleLogout}>
+      Logout
+    </Button>
+  );
 };
